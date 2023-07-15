85a22c7165a055699e3e73c5896cb149dd789eff
daevidvo <itsdavidvo@gmail.com>
2023-07-14 23:39:54 -0700

    removed style.css import from App.js since it wasn't being used

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import PageContainer from "./components/PageContainer";
 import "cirrus-ui";
-import "./style.css";
 import ParticlesRenderer from "./components/utils/ParticlesRenderer"
 
 function App() {
